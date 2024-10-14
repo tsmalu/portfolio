5611deb6c5c68e1e1167b1a4f4afcb10b7967e1d
maria luisa sousa <65040848+tsmalu@users.noreply.github.com>
2024-10-13 21:06:41 -0300

    atualização de arquivos
    responsividade no site

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ hamburger.addEventListener('click', () => {
   console.log("Menu hamburger clicado"); // Adiciona log para teste
     menu.classList.toggle('active'); // Alterna a classe 'active' para mostrar/esconder o menu
 });
+
+// Truncamento do texto
+const textElement = document.getElementById('truncated-text'); // Seleciona o elemento pelo ID
+const maxChars = 110; // Limite de caracteres desejado
+const originalText = textElement.innerText;
+
+if (originalText.length > maxChars) {
+  textElement.innerText = originalText.substring(0, maxChars) + '...'; // Trunca o texto
+}
