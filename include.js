b301422f13db64e0d93c94295ab93c1b4d514078
Malu <malu@MacBook-Pro-de-Malu.local>
2025-05-07 23:52:46 -0300

    componente de botão

--- include.js
+++ include.js
@@ -1,10 +1,22 @@
 function includeHTML() {
     document.querySelectorAll("[data-include]").forEach(el => {
-        fetch(el.getAttribute("data-include"))
-            .then(response => response.text())
-            .then(data => {
-                el.innerHTML = data;
-            });
+      const file = el.getAttribute("data-include");
+      const url = el.getAttribute("data-url");
+      const texto = el.getAttribute("data-texto");
+  
+      fetch(file)
+        .then(response => response.text())
+        .then(data => {
+          el.innerHTML = data;
+  
+          // Atualiza a URL e o texto do botão, se forem fornecidos
+          const link = el.querySelector("a");
+          if (link) {
+            if (url) link.setAttribute("href", url);
+            if (texto) link.textContent = texto;
+          }
+        });
     });
-}
-includeHTML();
+  }
+  includeHTML();
+  
